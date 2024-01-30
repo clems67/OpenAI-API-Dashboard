db54f1a936965b34572f3bb461cef6862eec2167
CARAVITA <clement.caravita@sogeti.com>
2024-01-30 16:17:40 +0100

    install MudBlazor

--- OpenAI-API-Dashboard/Program.cs
+++ OpenAI-API-Dashboard/Program.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using OpenAI_API_Dashboard;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
