b3c14556dba1ee68b49a91194cd9ae6549afaa98
CARAVITA <clement.caravita@sogeti.com>
2024-01-31 17:27:53 +0100

    store api key in local storage

--- OpenAI-API-Dashboard/Pages/Index.razor.cs
+++ OpenAI-API-Dashboard/Pages/Index.razor.cs
@@ -11,6 +11,8 @@ namespace OpenAI_API_Dashboard.Pages
         {
             using(var httpClient = new HttpClient())
             {
+                var key = await _localstorage.GetItemAsync<string>("OpenAiKey");
+                httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", key);
                 var result = await httpClient.GetAsync("https://api.openai.com/v1/models");
                 if(result.StatusCode == System.Net.HttpStatusCode.Unauthorized)
                 {
--- OpenAI-API-Dashboard/Program.cs
+++ OpenAI-API-Dashboard/Program.cs
@@ -2,12 +2,14 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using OpenAI_API_Dashboard;
 using MudBlazor.Services;
+using Blazored.LocalStorage;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddBlazoredLocalStorage();
 
 builder.Services.AddMudServices();
 
