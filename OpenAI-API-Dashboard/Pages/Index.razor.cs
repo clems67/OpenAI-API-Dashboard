be6adbd57f0a334289e03e35a0c5de1ba9d06551
CARAVITA <clement.caravita@sogeti.com>
2024-01-31 16:48:18 +0100

    tuto page

---
+++ OpenAI-API-Dashboard/Pages/Index.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Net.Http;
+using System.Net.Http.Json;
+
+namespace OpenAI_API_Dashboard.Pages
+{
+    public partial class Index
+    {
+        [Inject] public NavigationManager NavigationManager { get; set; } = default!;
+        private async Task GetLLMsModelsAsync()
+        {
+            using(var httpClient = new HttpClient())
+            {
+                var result = await httpClient.GetAsync("https://api.openai.com/v1/models");
+                if(result.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+                {
+                    NavigationManager.NavigateTo("/tuto");
+                }
+            }
+        }
+    }
+}
